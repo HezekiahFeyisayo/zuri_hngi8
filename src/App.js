ea3fb9e1f1897eb2933f6cad60aa21a9f4812399
Isaac Sokari <isaacsokari@gmail.com>
2020-08-05 18:20:18 +0100

    added initial files and folders

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TestComponent from './components/test.component';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/test.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './test.styles.scss'
+
+const TestComponent = () => (
+  <div>
+    <h2 className="test">This is the test component</h2>
+    <p className="text">Run 'npm i' or 'yarn' to install the necessary modules. <br/> The installed modules include: 
+    <li>react-router-dom</li>
+    <li>node-sass</li>
+    <li>bootstrap</li>
+    <li>react-boostrap</li>
+    </p>
+  </div>
+);
+
+export default TestComponent;
