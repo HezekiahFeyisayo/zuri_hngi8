96f4f3054e0f1560d2b3cd97e7d490cc424476b4
Lucas <gomez-lucas@live.com.ar>
2020-08-05 23:06:49 -0300

    Landing page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import TestComponent from './components/test.component';
+import Landing from './components/landing/Landing'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div  >
+  {/*    <h1>Hello World</h1> 
       <TestComponent />
+    */}  
+
+      <Landing />
     </div>
   );
 }
